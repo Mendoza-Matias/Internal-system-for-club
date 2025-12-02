aea8056dff110c6b573f685a537eee7f13aeb0c0
Mendoza Matias <mendoza.e.matias@gmail.com>
2025-12-02 12:09:28 -0300

    first commit

---
+++ smart_invoice-reminder/src/main/java/com/mmendoza/smart_invoice_reminder/SmartInvoiceReminderApplication.java
@@ -0,0 +1,13 @@
+package com.mmendoza.smart_invoice_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInvoiceReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInvoiceReminderApplication.class, args);
+	}
+
+}
---
+++ smart_invoice-reminder/src/test/java/com/mmendoza/smart_invoice_reminder/SmartInvoiceReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmendoza.smart_invoice_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInvoiceReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
